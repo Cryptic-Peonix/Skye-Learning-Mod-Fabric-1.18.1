ba7a6a610de6812bfa6a9da5afce61a99dabd54e
Skyla <skylaclark094@gmail.com>
2022-07-27 19:08:57 -0400

    Copper Shield

--- src/main/java/me/skyla/tutmod/core/items/ItemInit.java
+++ src/main/java/me/skyla/tutmod/core/items/ItemInit.java
@@ -1,9 +1,11 @@
 package me.skyla.tutmod.core.items;
 
+import com.github.crimsondawn45.fabricshieldlib.lib.object.FabricShieldItem;
 import me.skyla.tutmod.ExampleMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -13,6 +15,7 @@ public class ItemInit {
 
     public static final Item TIN_NUGGET = registerItem("tin_nugget", new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
 
+    public static final Item COPPER_SHIELD = registerItem("copper_shield", new FabricShieldItem(new FabricItemSettings().maxDamage(150).group(ItemGroup.COMBAT), 150, 4, Items.COPPER_INGOT));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(ExampleMod.MOD_ID, name), item);
